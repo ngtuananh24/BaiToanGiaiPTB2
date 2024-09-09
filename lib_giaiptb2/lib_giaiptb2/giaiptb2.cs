8344886d36c2d004e435ef094ba7fd73335300ff
ngtuananh24 <ngtuananh24072003@gmail.com>
2024-09-09 08:51:06 +0700

    Tao_thu_vien_tinh_ptb2

---
+++ lib_giaiptb2/lib_giaiptb2/giaiptb2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lib_giaiptb2
+{
+    public class giaiptb2
+    {
+        public static void GiaiPTB2(float a, float b, float c)
+        {
+            float delta = b * b - 4 * a * c;
+            if (delta < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("Phuong trinh co mo nghiem khep: x1 = x2 = {0}", (-b / (2*a)));
+            }
+            else
+            {
+                float x1, x2;
+                x1 = (float)(-b + Math.Sqrt(delta))/(2*a);
+                x2 = (float)(-b - Math.Sqrt(delta))/(2*a);
+                Console.WriteLine("Nghiem thu nhat la: {0}", x1);
+                Console.WriteLine("Nghiem thu hai la: {0}", x2);
+            }
+        }
+    }
+}
---
+++ testct/testct/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_giaiptb2;
+namespace testct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            giaiptb2.GiaiPTB2(1, -3, 2);
+            Console.ReadLine();
+        }
+    }
+}
