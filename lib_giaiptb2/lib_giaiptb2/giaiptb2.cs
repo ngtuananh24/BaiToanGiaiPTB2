3b28ade5f5e40b5dcc10f78c3b3ed6350c871d34
ngtuananh24 <ngtuananh24072003@gmail.com>
2024-09-09 21:14:48 +0700

    upload lib_giaiptb2

--- lib_giaiptb2/lib_giaiptb2/giaiptb2.cs
+++ lib_giaiptb2/lib_giaiptb2/giaiptb2.cs
@@ -10,19 +10,19 @@ namespace lib_giaiptb2
             if (a == 0) { return "day khong phai ptb2"; }
             if (delta < 0)
             {
-                return "Phuong trinh vo nghiem";
+                return "PTVN";
             }
             else if (delta == 0)
             {
                 float nghiemKep = -b / (2 * a);
                 nghiemKep = (float)((int)(nghiemKep * 100)) / 100;
-                return $"Phuong trinh co mot nghiem khep: x1 = x2 = {nghiemKep}";
+                return $"{nghiemKep}";
             }
             else
             {
                 float x1 = (float)((int)((-b + Math.Sqrt(delta)) / (2 * a) * 100)) / 100;
                 float x2 = (float)((int)((-b - Math.Sqrt(delta)) / (2 * a) * 100)) / 100;
-                return $"Phuong trinh co 2 nghiem phan biet: x1 = {x1}, x2 = {x2}";
+                return $"{x1},{x2}";
             }
         }
     }
